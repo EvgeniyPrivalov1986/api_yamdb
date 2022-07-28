445e4b73cbc20eb5a25067a9498e397316311ad6
Евгений Гребенчук <grebenchuk.evgeniy@gmail.com>
2022-07-28 08:56:05 +0500

    model Categories added

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import filters,viewsets
 
-# Create your views here.
+from reviews.models import Category
+from .permissions import IsAdminOrReadOnly
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name",)
+
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
-# Create your models here.
-print('hi2')
+
+class Category(models.Model):
+    name = models.CharField(
+        verbose_name='Категории (типы) произведений',
+        max_length=256,
+    )
+    slug = models.SlugField(
+        verbose_name='Уникальная строка идентификатор',
+        max_length=50,
+        unique=True,
+    )
+
+    def __str__(self) -> str:
+        return self.name[:15]

5e0df1d1859efcd7b41baf89e9eec45ef3ec70d1
Мокроусов Алексей <rocker9137@yandex.ru>
2022-07-31 11:10:27 +0500

    Modified views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,18 +3,24 @@ from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework import filters
 #from rest_framework.pagination import LimitOffsetPagination
-from reviews.models import Review, User
+from reviews.models import Review, Title, User
 from api.serializers import CommentSerializer, ReviewSerializer
 from api.permissions import AuthorOrAdminOrReadOnly
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
     permission_classes = (AuthorOrAdminOrReadOnly,)
 
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        title = get_object_or_404(Title, id=title_id)
+        return title.reviews.all()
+
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        title = get_object_or_404(Title, id=self.kwargs.get("title_id"))
+        serializer.save(author=self.request.user, title=title)
+
 
 
 class CommentViewSet(viewsets.ModelViewSet):

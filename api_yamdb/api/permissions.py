010390060612f6601ff717f1cc2fcb4e6ed1f422
Евгений Привалов <evprivalov86@gmail.com>
2022-07-31 14:07:07 +0500

    Auth v.2.5

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,6 +1,16 @@
 from rest_framework import permissions
 
 
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authentificated and (
+                request.user.is_superuser or request.user.is_admin
+            ))
+        )
+
+
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return (

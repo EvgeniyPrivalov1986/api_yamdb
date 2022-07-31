bae4f55afdd3c65f9252a973411225436fdbc85c
Евгений Привалов <evprivalov86@gmail.com>
2022-07-31 12:20:28 +0500

    Auth v.2

--- api_yamdb/reviews/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import User
-
-def profile(request):
-    users = User.objects.all()
-    return render(request, 'reviews/profile.html', {'users': users})

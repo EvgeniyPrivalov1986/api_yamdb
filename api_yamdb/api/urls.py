4a63ff254849c9b622697e5942367cb1c6afe402
Евгений Привалов <evprivalov86@gmail.com>
2022-08-02 19:34:14 +0500

    master v.2.10

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,42 +1,13 @@
-<<<<<<< HEAD
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet, GenreViewSet
 from users.views import APIUser, UserViewSetForAdmin
 
-app_name = 'api'
-
-router_v1 = DefaultRouter()
-router_v1.register('categories', CategoryViewSet)
-router_v1.register('genres', GenreViewSet)
-router_v1.register('users', UserViewSetForAdmin, basename='users')
+router = DefaultRouter()
+router.register('users', UserViewSetForAdmin, basename='users')
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls)),
     path('v1/auth/', include('users.urls')),
     path('v1/users/me/', APIUser.as_view(), name='me'),
-    path('v1/', include(router_v1.urls)),
+    path('v1/', include(router.urls)),
 ]
-=======
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-
-from .views import CategoryViewSet, GenreViewSet
-from users.views import APIUser, UserViewSetForAdmin
-
-app_name = 'api'
-
-router_v1 = DefaultRouter()
-router_v1.register('categories', CategoryViewSet)
-router_v1.register('genres', GenreViewSet)
-router_v1.register('users', UserViewSetForAdmin, basename='users')
-
-
-urlpatterns = [
-    path('v1/', include(router_v1.urls)),
-    path('v1/auth/', include('users.urls')),
-    path('v1/users/me/', APIUser.as_view(), name='me'),
-    path('v1/', include(router_v1.urls)),
-]
->>>>>>> 4d65b2308b8910108fac0dcdf8786a869173f68f
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import include, path
-=======
-from django.urls import path, include
->>>>>>> ec6a492bc25a313f0175de82980790c4c319a5cc
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -14,5 +10,4 @@ urlpatterns = [
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
-    path('api/', include('api.urls'))
 ]

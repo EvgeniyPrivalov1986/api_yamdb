1c253aef2420f5bbea7f410357a6525a4e521a35
Евгений Привалов <evprivalov86@gmail.com>
2022-08-02 20:26:01 +0500

    Auth 2.21

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,11 +3,11 @@ from rest_framework.routers import DefaultRouter
 
 from users.views import APIUser, UserViewSetForAdmin
 
-router = DefaultRouter()
-router.register('users', UserViewSetForAdmin, basename='users')
+router_v1 = DefaultRouter()
+router_v1.register('users', UserViewSetForAdmin, basename='users')
 
 urlpatterns = [
     path('v1/auth/', include('users.urls')),
     path('v1/users/me/', APIUser.as_view(), name='me'),
-    path('v1/', include(router.urls)),
+    path('v1/', include(router_v1.urls)),
 ]

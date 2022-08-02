c7db5ef750524a088f242156131b078842b73ff7
Евгений Привалов <evprivalov86@gmail.com>
2022-08-02 20:39:05 +0500

    Auth + CGT v.1

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Category, Genre
+from reviews.models import Category, Genre, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -15,3 +15,36 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
         exclude = ('id',)
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True     
+    )
+
+    class Meta:
+        model = Title,
+        fields = '__all__'
+
+
+class ReadTitleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(
+        read_only=True,
+        )
+    genre = GenreSerializer(
+        read_only=True,
+    )
+    # rating = serializers.IntegerField(
+    #     source='reviews__score__avg',
+    #     read_only=True,
+    # )
+
+    class Meta:
+        model = Title,
+        fields = '__all__'
